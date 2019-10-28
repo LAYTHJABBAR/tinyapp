95c53556ab7bd72c71f4ff7cd0c5d11b5ffb2c60
LAYTHJABBAR <layth.jabbar@yahoo.com>
2019-10-28 22:57:26 +0000

    server-short

--- express_server.js
+++ express_server.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-// app.get("/", (req, res) => {
-//   res.send("Hello layth!");
-// });
-// app.get("/urls.json", (req, res) => {
-//     res.json(urlDatabase);
-//   });
-// app.get("/hello", (req, res) => {
-//     res.send("<html><body>Hello <b>World</b></body></html>\n");
-//   });
+app.get("/", (req, res) => {
+  res.send("Hello layth!");
+});
+app.get("/urls.json", (req, res) => {
+    res.json(urlDatabase);
+  });
+app.get("/hello", (req, res) => {
+    res.send("<html><body>Hello <b>World</b></body></html>\n");
+  });
 app.get("/set", (req, res) => {
     const a = 1;
     res.send(`a = ${a}`);
@@ -24,6 +25,11 @@ app.get("/set", (req, res) => {
    app.get("/fetch", (req, res) => {
     res.send(`a = ${a}`);
    }); // is not accessible to a different get
+
+     app.get("/urls" , (req, res) => {
+         let tempplateVars = { urls: urlDatabase }
+      res.render("urls_index", tempplateVars)
+     })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
