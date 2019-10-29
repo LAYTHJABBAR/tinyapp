82e2f9db300232875a5ba8d404f432c9634020a4
LAYTHJABBAR <layth.jabbar@yahoo.com>
2019-10-29 00:04:44 +0000

    header

--- express_server.js
+++ express_server.js
@@ -3,34 +3,41 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-app.get("/", (req, res) => {
-  res.send("Hello layth!");
-});
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
-app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-  });
-app.get("/set", (req, res) => {
-    const a = 1;
-    res.send(`a = ${a}`);
-   });
+// app.get("/", (req, res) => {
+//   res.send("Hello layth!");
+// });
+// app.get("/urls.json", (req, res) => {
+//     res.json(urlDatabase);
+//   });
+// app.get("/hello", (req, res) => {
+//     res.send("<html><body>Hello <b>World</b></body></html>\n");
+//   });
+// app.get("/set", (req, res) => {
+//     const a = 1;
+//     res.send(`a = ${a}`);
+//    });
    
-   app.get("/fetch", (req, res) => {
-    res.send(`a = ${a}`);
-   }); // is not accessible to a different get
+//    app.get("/fetch", (req, res) => {
+//     res.send(`a = ${a}`);
+//    }); // is not accessible to a different get
 
      app.get("/urls" , (req, res) => {
          let tempplateVars = { urls: urlDatabase }
       res.render("urls_index", tempplateVars)
-     })
+     });
+     
+      app.get("/urls/:shortURL", (req, res) => {
+        let templateVars = { 
+            shortURL: req.params.shortURL, 
+            longURL:urlDatabase[req.params.shortURL] };
+        res.render("urls_show", templateVars);
+      }); 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-});
+}); 
